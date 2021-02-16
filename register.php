27ed0e359da9706a7e7797401f51d13475442e08
Toni Lukenda <toni.lukenda@gmail.com>
2021-02-16 19:06:02 +0100

    posodobljen dizajn registracije

--- register.php
+++ register.php
@@ -17,12 +17,42 @@
             <div class="col-lg-8 mx-auto">
                 <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
                 <form action="user_insert.php" method="post">
-                    <input type="text" name="first_name" placeholder="Vnesite ime" required="required"/> </br>
-                    <input type="text" name="last_name" placeholder="Vnesite priimek" required="required"/> </br>
-                    <input type="email" name="email" placeholder="Vnesite elektronsko pošto" required="required"/> </br>
-                    <input type="password" name="pass" placeholder="Vnesite geslo" required="required"/> </br>
-                    <input type="password" name="pass2" placeholder="Ponovite geslo" required="required"/> </br>
-                    <input type="submit" name="submit" value="Pošlji" /> 
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>Ime</label>
+                            <input class="form-control" type="text" name="first_name" placeholder="Vnesite ime"
+                                required="required" /> </br>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>Priimek</label>
+                            <input class="form-control" type="text" name="last_name" placeholder="Vnesite priimek" required="required" />
+                            </br>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>E-pošta</label>
+                            <input class="form-control" type="email" name="email" placeholder="Vnesite elektronsko pošto"
+                                required="required" /> </br>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>Geslo</label>
+                            <input class="form-control" type="password" name="pass" placeholder="Vnesite geslo" required="required" /> </br>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                            <label>Ponovi geslo</label>
+                            <input class="form-control" type="password" name="pass2" placeholder="Ponovite geslo" required="required" />
+                            </br>
+                        </div>
+                    </div>
+                    </br>
+                    <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Pošlji</button></div>
                 </form>
             </div>
         </div>
