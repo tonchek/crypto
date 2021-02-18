4d4db5a811801ea4bea97ca587eff7f4c2402ad9
Toni Lukenda <toni.lukenda@gmail.com>
2021-02-18 19:31:23 +0100

    dodajanje kripto valute

--- header.php
+++ header.php
@@ -37,10 +37,10 @@ include_once "session.php";
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
-                            href="users.php">Uporabniki</a></li>
+                            href="cryptocurrencies.php">Valute</a></li>
                     <?php
                             if(isset($_SESSION['user_id'])) {
-                                $first_name = $_SESSION        
+                                $first_name = $_SESSION;        
 
                                 echo '<li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
                                 href="logout.php">Odjava ('.$first_name.')</a></li>';
